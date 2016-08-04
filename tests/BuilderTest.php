c2f3bcb7c60f19c21b1fc64951aa238c8ebe1a88
Álvaro Guimarães <alvaroguimaraes@gmail.com>
2016-08-04 16:01:49 -0300

    New style rule.

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -10,6 +10,7 @@ class BuilderTest extends PHPUnit_Framework_TestCase
     {
         $collection = (new Builder())->setTotal(3)
             ->generate();
+
         $this->assertEquals(3, count($collection));
     }
 
@@ -18,8 +19,10 @@ class BuilderTest extends PHPUnit_Framework_TestCase
         $collection = (new Builder())->setStep(2)
             ->setTotal(8)
             ->generate();
+
         $firstTurn = new SpiralPoint(2, 1);
         $secondTurn = new SpiralPoint(1, 2);
+
         $this->assertEquals($firstTurn, $collection[3]);
         $this->assertEquals($secondTurn, $collection[5]);
     }
