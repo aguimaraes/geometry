3d674745007c19287606c380a9af9a132f646e38
Álvaro Guimarães <alvaroguimaraes@gmail.com>
2016-08-03 23:36:43 -0300

    Allowing diagonal movement.

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Aguimaraes\Spiral\Builder;
+use Aguimaraes\Spiral\Exceptions\SpiralException;
 use Aguimaraes\Spiral\SpiralPoint;
 
 class BuilderTest extends PHPUnit_Framework_TestCase
@@ -19,6 +20,34 @@ class BuilderTest extends PHPUnit_Framework_TestCase
             ->generate();
         $firstTurn = new SpiralPoint(2, 1);
         $secondTurn = new SpiralPoint(1, 2);
+        $this->assertEquals($firstTurn, $collection[3]);
+        $this->assertEquals($secondTurn, $collection[5]);
+    }
+
+    public function testAngleCannotBeGreaterThan90()
+    {
+        $this->expectException(SpiralException::class);
+        $builder = new Builder();
+        $builder->setAngle(180);
+    }
+
+    public function testAngleMustBeMultipleOf45()
+    {
+        $this->expectException(SpiralException::class);
+        $builder = new Builder();
+        $builder->setAngle(70);
+    }
+
+    public function testIfAngleWillBeRespected()
+    {
+        $collection = (new Builder())->setStep(2)
+            ->setAngle(45)
+            ->setTotal(8)
+            ->generate();
+
+        $firstTurn = new SpiralPoint(3, 1);
+        $secondTurn = new SpiralPoint(5, 3);
+
         $this->assertEquals($firstTurn, $collection[3]);
         $this->assertEquals($secondTurn, $collection[5]);
     }
